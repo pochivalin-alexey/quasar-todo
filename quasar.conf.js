e28f863f66fe3711059ad8490b3886780651905f
Pochivalin Alexey <gorchok@mail.ru>
2020-06-26 16:21:45 +0300

    change public path url

--- quasar.conf.js
+++ quasar.conf.js
@@ -60,7 +60,7 @@ module.exports = function(/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "history", // available values: 'hash', 'history'
-      publicPath: "quasar-todo/dist",
+      publicPath: "quasar-todo/dist/spa",
 
       // rtl: false, // https://quasar.dev/options/rtl-support
       // showProgress: false,
