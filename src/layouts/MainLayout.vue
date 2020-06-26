518923719387775f94de0df793c2b969697caea1
Pochivalin Alexey <gorchok@mail.ru>
2020-06-26 16:10:37 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-orange-3">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDay }}</div>
+      </div>
+      <q-img src="statics/header.jpg" class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="250"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>Todo</q-item-section>
+          </q-item>
+          <q-item to="/help" clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>Help</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="statics/drawer.jpg"
+        style="height: 192px;"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+          <div class="text-weight-bold">Mr. Smith</div>
+          <div>smit@info.ru</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+
+export default {
+  name: "MainLayout",
+
+  components: {},
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+  computed: {
+    todaysDay() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "DD.MM.YYYY");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+}
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+      dignissimos quisquam necessitatibus excepturi ea eligendi nam. Quidem vel
+      quaerat alias ullam, odit assumenda quas dolore perferendis minima amet
+      quibusdam provident.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat mollitia
+      id fuga consectetur provident facilis atque totam explicabo obcaecati. Id,
+      amet quasi? Ex doloremque voluptate ipsa rem numquam voluptatum a.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat mollitia
+      id fuga consectetur provident facilis atque totam explicabo obcaecati. Id,
+      amet quasi? Ex doloremque voluptate ipsa rem numquam voluptatum a.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat mollitia
+      id fuga consectetur provident facilis atque totam explicabo obcaecati. Id,
+      amet quasi? Ex doloremque voluptate ipsa rem numquam voluptatum a.
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
